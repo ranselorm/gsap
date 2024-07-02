8f6559b0b1dd540c6ec9e3f995385d8a91f28947
Selorm <ranselorm@outlook.com>
2024-07-02 18:54:02 +0000

    Animate element with GsapFromTo

--- src/pages/GsapFromTo.jsx
+++ src/pages/GsapFromTo.jsx
@@ -1,5 +1,28 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFromTo = () => {
   // TODO: Implement the gsap.fromTo() method
+
+  useGSAP(() => {
+    gsap.fromTo(
+      "#red-box",
+      {
+        x: 0,
+        borderRadius: "30%",
+        rotation: 0,
+      },
+      {
+        x: 300,
+        repeat: -1,
+        borderRadius: "40%",
+        yoyo: true,
+        rotation: 360,
+        duration: 2,
+        ease: "bounce.out",
+      }
+    );
+  }, []);
 
   return (
     <main>
